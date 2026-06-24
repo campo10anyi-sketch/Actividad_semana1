daa5f6a3a7ea3796ed820a73530e8d2fd442094b
campo10anyi-sketch <tu_correo_de_github@gmail.com>
2026-06-24 14:55:20 -0500

    actividad semana 6

---
+++ modulo 3/Actividad semana 6/server.js
@@ -0,0 +1,22 @@
+const express = Require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/incidencia', (req, res) => {
+
+  const tipo = req.body.tipo;
+  const descripcion = req.body.descripcion;
+
+  res.json({
+    mensaje: "Incidencia registrada",
+    tipo: tipo,
+    descripcion: descripcion
+  });
+
+});
+
+ 
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto http://localhost:3000/registro');
+});
